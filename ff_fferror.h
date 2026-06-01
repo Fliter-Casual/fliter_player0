8d41e5d62743889ef94cbf00a71097a27831e74f
Fliter-Casual <asd632979741@qq.com>
2026-06-01 21:19:15 +0800

    feat: 新增 ff_fferror.h 定义基础错误码
    1. 添加失败、内存不足、非法状态及空指针等通用错误宏
    2. 统一项目内的错误返回标识

---
+++ ff_fferror.h
@@ -0,0 +1,10 @@
+#ifndef FF_FFERROR_H
+#define FF_FFERROR_H
+
+// 错误码
+#define EIJK_FAILED             -1 // 失败
+#define EIJK_OUT_OF_MEMORY      -2 // 内存不足
+#define EIJK_INVALID_STATE      -3 // 非法状态
+#define EIJK_NULL_IS_PTR        -4 // 空指针
+
+#endif // FF_FFERROR_H
