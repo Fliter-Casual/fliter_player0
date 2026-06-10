f2595ec01e4f37b25b3627a00503309d4c802a9e
Fliter-Casual <asd632979741@qq.com>
2026-06-10 14:00:06 +0800

    fix(audio): 修复 Windows 下因 XAudio2 失败导致的无声问题
    - 问题：部分 Windows 电脑上报错 "XAudio2Create() failed"，导致 SDL 音频设备初始化失败。
    - 解决：在 SDL 初始化前设置环境变量 `SDL_AUDIODRIVER=directsound`。
    - 原因：DirectSound 比默认的 XAudio2 后端具有更好的硬件和驱动兼容性。
    - 结果：解决了特定环境下的无声问题，确保音频播放稳定性。

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
+#include <stdio.h>
 #include "mainwindow.h"
-
 #include <QApplication>
+
+// 在 FFPlayer::ffplayer_create() 或 main() 的最开始调用，DirectSound 对老旧硬件和不同驱动程序的兼容性极好，几乎在所有 Windows 电脑上都能工作
+void force_sdl_directsound()
+{
+    // Windows 下设置环境变量
+    #ifdef _WIN32
+        _putenv("SDL_AUDIODRIVER=directsound");
+    #else
+        setenv("SDL_AUDIODRIVER", "directsound", 1);
+    #endif
+}
 
 #undef main
 int main(int argc, char *argv[])
