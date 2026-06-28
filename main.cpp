4a71fdb8233a6204460c9a3bbad0a747d6976170
Fliter-Casual <asd632979741@qq.com>
2026-06-28 20:03:20 +0800

    配置 easylogging++ 日志系统，设置日志格式、输出位置和日志级别

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include "mainwindow.h"
 #include <QApplication>
+#include "log/easylogging++.h"
+
+INITIALIZE_EASYLOGGINGPP    // 初始化宏，有且只能使用一次
 
 // 在 FFPlayer::ffplayer_create() 或 main() 的最开始调用，DirectSound 对老旧硬件和不同驱动程序的兼容性极好，几乎在所有 Windows 电脑上都能工作
 void force_sdl_directsound()
@@ -16,6 +19,24 @@ void force_sdl_directsound()
 #undef main
 int main(int argc, char *argv[])
 {
+    //    el::Loggers::reconfigureAllLoggers(el::ConfigurationType::Format, "%datetime %level %func(L%line) %msg");
+
+    el::Configurations conf;
+    conf.setToDefault();
+    conf.setGlobally(el::ConfigurationType::Format, "[%datetime | %level] %func(L%line) %msg");
+    conf.setGlobally(el::ConfigurationType::Filename, "log_%datetime{%Y%M%d}.log");
+    conf.setGlobally(el::ConfigurationType::Enabled, "true");
+    conf.setGlobally(el::ConfigurationType::ToFile, "true");
+    el::Loggers::reconfigureAllLoggers(conf);
+    el::Loggers::reconfigureAllLoggers(el::ConfigurationType::ToStandardOutput, "true"); // 也输出一份到终端
+
+    //    LOG(VERBOSE) << "logger test"; //该级别只能用宏VLOG而不能用宏 LOG(VERBOSE)
+    LOG(TRACE) << " logger";
+    //    LOG(DEBUG) << "logger test";
+    LOG(INFO) << "logger test";
+    LOG(WARNING) << "logger test";
+    LOG(ERROR) << "logger test";
+
     force_sdl_directsound();
     QApplication a(argc, argv);
     MainWindow w;
